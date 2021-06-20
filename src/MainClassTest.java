ce34fc135e966255bf7d0085e4c22f815d902f59
vnartov <stanly744@yandex.ru>
2021-06-20 14:18:38 +0300

    I_Intro_Java_JUnit_Test_3

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -3,8 +3,9 @@ import org.junit.Test;
 
 public class MainClassTest extends MainClass {
     @Test
-    public void testGetClassNumber() {
-        int a = getClassNumber();
-        Assert.assertTrue("Число меньше 45", a > 45);
+    public void testGetClassString() {
+        String a = getClassString();
+        String hello = "ello";
+        Assert.assertTrue("В сторке нет слов Hello или hello", a.contains(hello));
     }
 }
